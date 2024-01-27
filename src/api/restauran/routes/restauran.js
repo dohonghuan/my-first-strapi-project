d3fdad03ab651a7989334edab4a9cae36d6897d2
dohonghuan <dohonghuan@gmail.com>
2024-01-27 13:24:04 +0700

    Initial commit

---
+++ src/api/restauran/routes/restauran.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restauran router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::restauran.restauran');
---
+++ src/api/restauran/services/restauran.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restauran service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::restauran.restauran');
